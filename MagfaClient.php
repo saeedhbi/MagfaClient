8322b5b697cb45b120e99c729994e42bf8dd349a
Saeed Habibi <saeeditce.habibi@gmail.com>
2014-06-15 12:24:29 +0430

    Magfa Client

--- MagfaClient.php
+++ MagfaClient.php
@@ -1,6 +1,15 @@
 <?php
 namespace MagfaClient;
 
+use SoapClient;
+
 class MagfaClient
 {
+    public function __construct($wsdl){
+        $this->connect($wsdl);
+    }
+    
+    public function connect($wsdl){
+        return new SoapClient($wsdl);
+    }
 }
