3be46f49628fbb61a4af1a1139ca5d064196d298
Saeed Habibi <saeeditce.habibi@gmail.com>
2014-06-15 13:17:59 +0430

    Magfa Client update

--- MagfaClient.php
+++ MagfaClient.php
@@ -5,11 +5,23 @@ use SoapClient;
 
 class MagfaClient
 {
+    public $connection;
+    
     public function __construct($wsdl){
-        $this->connect($wsdl);
+        $this->connection = $this->connect($wsdl);
     }
     
-    public function connect($wsdl){
-        return new SoapClient($wsdl);  
+    public function connect($wsdl, $login){
+        return new SoapClient($wsdl, $login);  
+    }
+    
+    public function enqueue($params){ 
+        $params = array(
+            'domain'=>$this->datasets['domain'],
+            'messageBodies'=>$this->datasets['messageBodies'],
+            'recipientNumbers'=>$this->datasets['recipientNumbers'],
+            'senderNumbers'=>$this->datasets['30004866000006']
+        );
+        return $result = $this->connection->__soapCall('enqueue', $params);
     }
 }
