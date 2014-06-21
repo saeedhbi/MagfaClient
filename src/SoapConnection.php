2d26fde8f65926118b3aceba70b900c4b92aa760
Saeed Habibi <saeeditce.habibi@gmail.com>
2014-06-21 10:25:07 +0430

    SoapConnection new service of magfa.

--- src/SoapConnection.php
+++ src/SoapConnection.php
@@ -1,7 +1,7 @@
 <?php
 namespace MagfaClient;
 
-use Exceptions\AsanakSOAPException;
+use Exceptions\MagfaSOAPException;
 use ConnectionInterface;
 
 class SoapConnection implements ConnectionInterface
@@ -47,7 +47,7 @@ class SoapConnection implements ConnectionInterface
                                 );
                 $this->connection = new SoapClient($connect['wsdl'], $params);
             } catch (SoapFault $e) {
-                throw new AsanakSOAPException($e->faultstring, 401);
+                throw new MagfaSOAPException($e->faultstring, 401);
             }
         }
         return $this->connection;
@@ -61,86 +61,131 @@ class SoapConnection implements ConnectionInterface
     public function sendSms()
     {
         try {
-            $s = $this->connection->sendSms(array(
-                'userCredential' => array(
-                    'username' => $this->datasets['username'],
-                    'password' => $this->datasets['password']
+            $s = $this->connection->__soapCall('enqueue', array(
+                'domain' => $this->datasets['domain'],
+                'messageBodies' => array(
+                    $this->datasets['messageBodies']
                 ),
-                'srcAddresses' => $this->datasets['srcAddresses'],
-                'destAddresses' => $this->datasets['destAddresses'],
-                'msgBody' => $this->datasets['msgBody'],
-                'msgEncoding' => $this->datasets['msgEncoding']
+                'recipientNumbers' => $this->datasets['recipientNumbers'],
+                'senderNumbers' => array(
+                    $this->datasets['senderNumbers']
+                )
             ));
             return $s;
         } catch (SoapFault $e) {
-            throw new AsanakSOAPException($e->faultstring, 401);
+            throw new MagfaSOAPException($e->faultstring, 401);
         }
     }
 
     /**
-     * Class to getReportByMsgId from provider
+     * Class to getCredit from provider
      *
      * @return object
      */
-    public function getReportByMsgId()
+    public function getCredit()
     {
         try {
-            $s = $this->connection->getReportByMsgId(array(
-                'userCredential' => array(
-                    'username' => $this->datasets['username'],
-                    'password' => $this->datasets['password']
-                ),
-                'msgIds' => $this->datasets['msgIds']
+            $s = $this->connection->__soapCall('getCredit', array(
+                'domain' => $this->datasets['domain']
             ));
+            
             return $s;
         } catch (SoapFault $e) {
-            throw new AsanakSOAPException($e->faultstring, 401);
+            throw new MagfaSOAPException($e->faultstring, 401);
         }
     }
 
     /**
-     * Class to getReceivedMsg from provider
+     * Class to getAllMessages from provider
      *
      * @return object
      */
-    public function getReceivedMsg()
+    public function getAllMessages()
     {
         try {
-            $s = $this->connection->getReceivedMsg(array(
-                'userCredential' => array(
-                    'username' => $this->datasets['username'],
-                    'password' => $this->datasets['password']
-                ),
-                'wsdl' => $this->datasets['wsdl'],
-                'srcAddresses' => $this->datasets['srcAddresses'],
-                'destAddresses' => $this->datasets['destAddresses'],
-                'maxReturnedMsg' => $this->datasets['maxReturnedMsg'],
-                'fromTime' => $this->datasets['fromTime']
+            $s = $this->connection->__soapCall('getAllMessages', array(
+                'domain' => $this->datasets['domain'],
+                'numberOfMessasges' => $this->datasets['numberOfMessasges']
             ));
+            
             return $s;
         } catch (SoapFault $e) {
-            throw new AsanakSOAPException($e->faultstring, 401);
+            throw new MagfaSOAPException($e->faultstring, 401);
         }
     }
 
     /**
-     * Class to getUserCredit from provider
+     * Class to getAllMessagesWithNumber from provider
      *
      * @return object
      */
-    public function getUserCredit()
+    public function getAllMessagesWithNumber()
     {
         try {
-            $s = $this->connection->getUserCredit(array(
-                'userCredential' => array(
-                    'username' => $this->datasets['username'],
-                    'password' => $this->datasets['password']
-                ),
-                'wsdl' => $this->datasets['wsdl']
+            $s = $this->connection->__soapCall('getAllMessagesWithNumber', array(
+                'domain' => $this->datasets['domain'],
+                'numberOfMessasges' => $this->datasets['numberOfMessasges'],
+                'destNumber' => $this->datasets['destNumber']
             ));
+            
             return $s;
         } catch (SoapFault $e) {
-            throw new AsanakSOAPException($e->faultstring, 401);
+            throw new MagfaSOAPException($e->faultstring, 401);
+        }
+    }
+    
+    /**
+     * Class to getMessageId from provider
+     *
+     * @return object
+     */
+    public function getMessageId()
+    {
+        try {
+            $s = $this->connection->__soapCall('getMessageId', array(
+                'domain' => $this->datasets['domain'],
+                'checkingMessageId' => $this->datasets['checkingMessageId']
+            ));
+    
+            return $s;
+        } catch (SoapFault $e) {
+            throw new MagfaSOAPException($e->faultstring, 401);
+        }
+    }
+    
+    /**
+     * Class to getMessageStatus from provider
+     *
+     * @return object
+     */
+    public function getMessageStatus()
+    {
+        try {
+            $s = $this->connection->__soapCall('getMessageStatus', array(
+                'messageId' => $this->datasets['messageId']               
+            ));
+    
+            return $s;
+        } catch (SoapFault $e) {
+            throw new MagfaSOAPException($e->faultstring, 401);
+        }
+    }
+    
+    /**
+     * Class to getMessageStatuses from provider
+     *
+     * @return object
+     */
+    public function getMessageStatuses()
+    {
+        try {
+            $s = $this->connection->__soapCall('getMessageStatuses', array(
+                'messageId' => $this->datasets['messageId']               
+            ));
+    
+            return $s;
+        } catch (SoapFault $e) {
+            throw new MagfaSOAPException($e->faultstring, 401);
         }
     }
 }
